f24c7d2b67f241394c7546dd7a374cd35a95a2f9
OMEGA-Y <science_co@naver.com>
2021-01-15 16:52:55 +0900

    [#5] api - user.js에 cart api 추가 및 bodyParser 설치

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var projectRouter = require('./routes/project');
@@ -33,6 +34,9 @@ app.use('/api', indexRouter);
 app.use('/api/project', projectRouter);
 app.use('/api/user', userRouter);
 app.use('/api/category', categoryRouter);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
