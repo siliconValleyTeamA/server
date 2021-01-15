b2705453655d8f9191bc32d2506250617452f703
유 창헌 <dbckdgjs1130@naver.com>
2021-01-15 18:17:55 +0900

    [#2] history, point, funding a project

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const userData = require("../mocks/user.json");
 const goodsData = require("../mocks/goods.json");
+const historyData = require("../mocks/history.json");
+const jjimData = require("../mocks/jjim.json");
 
 let cartList = [];
 let jjimList = [{ id: 1 }, { id: 2 }];
@@ -22,6 +24,25 @@ router.delete("/jjim", function (req, res, next) {
   const projectId = req.body.projectId;
   jjimList = jjimList.filter((jjim) => jjim.id != projectId);
   res.json(jjimList);
+});
+
+router.get("/jjim",function(req, res, next) {
+  res.json(jjimData);
+});
+
+router.get("/history",function(req, res, next) {
+  res.json(historyData);
+});
+
+router.post("/fundings",function(req, res, next) {
+  const projectId=req.body.projectId;
+  const point =req.body.point;
+  res.json(projectId,point);
+});
+
+router.post("/points",function(req, res, next) {
+  const point= req.body.point;
+  res.json(point);
 });
 
 module.exports = router;
