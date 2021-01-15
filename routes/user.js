9b91fd5d705d6661a82839fb26a2701197ca60d7
OMEGA-Y <science_co@naver.com>
2021-01-15 16:52:55 +0900

    [#5] api - user.js에 cart api 추가 및 bodyParser 설치

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,59 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const utils = require("../utils.js");
+const utils = require('../utils.js');
 
-const userData = require("../mocks/user.json");
-const jjimData = require("../mocks/jjim.json");
-const goodsData = require("../mocks/goods.json");
+const userData = require('../mocks/user.json');
+const jjimData = require('../mocks/jjim.json');
+const goodsData = require('../mocks/goods.json');
+const cartData = require('../mocks/cart.json');
 
 let cartList = [];
 let jjimList = [{ id: 1 }, { id: 2 }];
 
-router.get("/point", function (req, res, next) {
+router.get('/point', function (req, res, next) {
   res.json(userData);
 });
 
-router.post("/cart", function (req, res, next) {
-  const projectId = req.body.projectId;
-  const insertData = goodsData.find((goods) => goods.id == projectId);
-  cartList.push(insertData);
-  res.json({ success: true });
-});
-
-router.delete("/jjim", function (req, res, next) {
+router.delete('/jjim', function (req, res, next) {
   const projectId = req.body.projectId;
   jjimList = jjimList.filter((jjim) => jjim.id != projectId);
   res.json(jjimList);
 });
 
-router.get("/jjim", async function(req, res, next){
+router.get('/carts', function (req, res, next) {
+  res.json(cartList);
+});
+
+router.post('/carts', function (req, res, next) {
+  const projectId = parseInt(req.body.projectId, 10);
+  const insertData = goodsData.find((goods) => goods.id === projectId);
+  cartData.push(insertData);
+  res.json({ success: true });
+});
+
+router.delete('/carts', function (req, rest, next) {
+  const projectId = parseInt(req.body.projectId, 10);
+  const goodsIdx = cartData.findIndex((goods) => goods.id === projectId);
+  if (goodsIdx === -1) {
+    return res.status(404).josn({ error: 'Unknown goods' });
+  }
+  cartData.splice(goodsIdx, 1);
+  res.json({ success: true });
+});
+
+router.put('/carts', function (req, res, next) {
+  const projectId = parseInt(req.body.projectId, 10);
+  const changeData = cartData.find((goods) => goods.id === projectId);
+  changeData.point = req.body.point;
+  res.json({ success: true });
+});
+
+router.get('/jjim', async function (req, res, next) {
   var sortjjimData = jjimData.sort(utils.sortByJjimTime);
   res.json(sortjjimData);
 });
 
-router.post("/jjim", function(req, res, next){
+router.post('/jjim', function (req, res, next) {
   const projectId = req.body.projectId;
   const getGoodsData = goodsData.find((goods) => goods.id == projectId);
   const inserData = utils.createJjimData(getGoodsData, jjimData.length + 1);
@@ -39,8 +61,8 @@ router.post("/jjim", function(req, res, next){
   res.json(jjimData);
 });
 
-router.post("/logout", function(req, res, next){
-  res.json({ test: "success" });
+router.post('/logout', function (req, res, next) {
+  res.json({ test: 'success' });
 });
 
 module.exports = router;
