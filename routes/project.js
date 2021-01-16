6190f083f794e5b60b0642b10c88023b4099a839
nohgijin <ynda11111@naver.com>
2021-01-16 22:34:35 +0900

    [#15] fix - 펀딩 내역 추가 api 작성

--- routes/project.js
+++ routes/project.js
@@ -10,18 +10,22 @@ const {
   getUserJjim,
 } = require("../db/project");
 
+//곧 개봉될 프로젝트 조회
 router.get("/schedule", async function (req, res, next) {
   res.json(await getScheduleProject());
 });
 
+//곧 성골할 프로젝트 조회
 router.get("/success", async function (req, res, next) {
   res.json(await getSoonSuccessProject());
 });
 
+//인기있는 프로젝트 조회
 router.get("/popular", async function (req, res, next) {
   res.json(await getPopularProject());
 });
 
+//프로젝트를 카테고리, 필터별로 조회
 router.get(
   "/category/:categoryId/:filterType",
   async function (req, res, next) {
@@ -31,16 +35,17 @@ router.get(
       category: category,
       filterType: filterType,
     });
-
     res.json(rows);
   }
 );
 
+//프로젝트 상세내용 조회
 router.get("/:projectId", async function (req, res, next) {
   const projectId = req.params.projectId;
   res.json(await getProjectDetail({ projectId }));
 });
 
+//프로젝트 찜 여부 조회
 router.get("/:projectId/jjim", async function (req, res, next) {
   const projectId = req.params.projectId;
   const userId = 3;
