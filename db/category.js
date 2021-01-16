47ef68a7f9a8b044d07bd34b10b74e4b9c730547
nohgijin <ynda11111@naver.com>
2021-01-16 20:46:36 +0900

    [#13] fix - jjim api 제작

--- db/category.js
+++ db/category.js
@@ -1,6 +1,7 @@
 const connection = require("./connection");
 const mysql = require("mysql2");
 
+//카테고리 조회
 async function getCategory() {
   let [rows, fields] = await connection.query(`SELECT * FROM category`);
   return rows;
