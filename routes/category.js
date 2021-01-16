2bdc5d559fbdd8915c249bdd2279674931e8d26d
nohgijin <ynda11111@naver.com>
2021-01-16 22:37:30 +0900

    [#15] chore - 주석 추가

--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const { getCategory } = require("../db/category");
 
+//모든 카테고리 조회
 router.get("/", async function (req, res, next) {
   res.json(await getCategory());
 });
