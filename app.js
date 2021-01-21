666f2c48eb556564a8d0540357228facd87d31bc
유 창헌 <dbckdgjs1130@naver.com>
2021-01-21 21:42:24 +0900

    [#20] -refactor 상품올리기 미리보기 폴더 경로설정정

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use("/api/project", projectRouter);
 app.use("/api/user", userRouter);
 app.use("/api/category", categoryRouter);
 app.use("/auth", authRouter);
+app.use('/uploads', express.static('uploads'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
