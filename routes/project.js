22a20cf59d4c295c4d1719e3ca7ef50325ad1cfb
유 창헌 <dbckdgjs1130@naver.com>
2021-01-21 21:44:53 +0900

    [#20] refactor- 프로젝트 db 연결

--- routes/project.js
+++ routes/project.js
@@ -8,8 +8,38 @@ const {
   getCategoryProject,
   getProjectDetail,
   getUserJjim,
+  addProject,
 } = require("../db/project");
 
+const multer =require('multer');
+const connection = require("../db/connection");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${file.originalname}`)
+  },
+  fileFilter: (req, file, cb) => {
+      const ext = path.extname(file.originalname)
+      if (ext !== '.jpg' || ext !== '.png') {
+          return cb(res.status(400).end('only jpg, png are allowed'), false);
+      }
+      cb(null, true)
+  }
+})
+
+var upload = multer({ storage: storage }).single("file")
+
+router.post("/uploadimage", async function (req, res,next){
+  upload(req, res, err => {
+    if (err) {
+        return res.json({ success: false, err })
+    }
+    return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
+})
+});
 //곧 개봉될 프로젝트 조회
 router.get("/schedule", async function (req, res, next) {
   res.json(await getScheduleProject());
@@ -52,6 +82,19 @@ router.get("/:projectId/jjim", async function (req, res, next) {
   const rows = await getUserJjim({ projectId, userId });
   if (rows.length != 0) res.json(rows[0]);
   else res.json({ success: false });
+});
+
+router.post("/uploadproject", async function(req ,res ,next){
+  const title= req.body.title;
+  const company= req.body.company;
+  const goalmoney= req.body.goalmoney;
+  const start_date= req.body.start_date;
+  const end_date= req.body.end_date;
+  const description= req.body.description;
+  const price= req.body.price;
+  const images= req.body.images;
+  const category= req.body.category; 
+  addProject({title, company, goalmoney,start_date,end_date,category,images,description});
 });
 
 module.exports = router;
