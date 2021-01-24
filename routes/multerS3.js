7760e572660bd83bd74e2b5a2bc0151411c87147
OMEGA-Y <science_co@naver.com>
2021-01-24 20:45:06 +0900

    [#36] refactor - 이미지 업로드 관련 파일 분리

---
+++ routes/multerS3.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const s3connect = require("./s3config");
+const s3 = new aws.S3(s3connect);
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "fuding-bucket",
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(
+        null,
+        Math.floor(Math.random() * 1000).toString() +
+          Date.now() +
+          "." +
+          file.originalname.split(".").pop()
+      );
+    },
+  }),
+  limits: {
+    fileSize: 1000 * 1000 * 10,
+  },
+}).single("file");
+
+module.exports = { upload };
