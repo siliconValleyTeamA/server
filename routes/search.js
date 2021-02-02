4a717600747c9d09f01f06fba869a1f47e6b9393
유 창헌 <dbckdgjs1130@naver.com>
2021-02-03 02:05:14 +0900

    [#55] fix- 엘라스틱 서치 연동 중

--- routes/search.js
+++ routes/search.js
@@ -3,11 +3,35 @@ var router = express.Router();
 const client = require('./server.client');
 // const { getCategory } = require("../db/category");
 
+
+client.ping({
+    requestTimeout: 30000,
+  }, function (error) {
+    error
+      ? console.error('ElasticSearch cluster is down!')
+      : console.log('ElasticSearch is ok');
+  });
+
+
+function ElasticSearchClient(body) {
+    // perform the actual search passing in the index, the search query and the type
+    //console.log(JSON.parse(body.search));
+    
+  return client.search({index: 'project', body: body});
+}
+
 //모든 카테고리 조회
 router.post("/", async function (req, res, next) {
-  //console.log(req.body);
-//   console.log(res.json(client.search({index:"title",body:req.body})))
-    return client.search({index: 'title', body: req.body})
+  console.log(req.body);
+  //console.log(client.search({index: 'title', body: `"${req.body.search}"`}))
+  ElasticSearchClient(req.body).then((result)=>{res.json(result.hits.hits);}); 
 });
+
+
+
+// function ElasticSearchClient(body) {
+//     // perform the actual search passing in the index, the search query and the type
+//     console.log(client.search({index: 'title', body: body}));
+// }
 
 module.exports = router;
